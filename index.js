598170494e474523543efabdc50cba9aa7cf5920
iRoge <45058118+iRoge@users.noreply.github.com>
2020-11-28 16:37:02 +0300

    Сделал открытие xlsx файла

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const http = require('http');
 const puppeteer = require('puppeteer');
+const xl = require('exceljs');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
-});
+const server = http.createServer();
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    start();
 });
+
+function start() {
+    (async () => {
+        await analizeSites();
+    })()
+}
+
+async function analizeSites() {
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+    const page = await browser.newPage();
+
+    const workbook = new xl.Workbook();
+    await workbook.xlsx.readFile('file.xlsx');
+    let worksheet = workbook.getWorksheet(1);
+
+    let urlRow = 4;
+    let siteUrl = worksheet.getCell(urlRow,3).toString();
+
+    while (siteUrl) {
+        await page.goto(siteUrl);
+        await page.waitFor(3000);
+        urlRow++;
+        siteUrl = worksheet.getCell(urlRow,3).toString();
+    }
+
+}
