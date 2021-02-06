7891cb7ae6662152a50968af9257f489377c8fda
iRoge <45058118+iRoge@users.noreply.github.com>
2021-02-06 14:26:33 +0300

    finally

--- acmodasiParser.js
+++ acmodasiParser.js
@@ -2,6 +2,7 @@ const http = require('http');
 const puppeteer = require('puppeteer');
 const xl = require('exceljs');
 const fs = require('fs');
+const mailer = require('nodemailer');
 
 const hostname = 'localhost';
 const port = 3001;
@@ -36,15 +37,17 @@ async function analizeSites() {
         }
     });
     const page = await browser.newPage();
+    const countOfPages = 68;
     await page.setDefaultNavigationTimeout(0);
-    page.on('console', msg => {
-        console.log(msg.text());
-    });
+
+    // page.on('console', msg => {
+    //     console.log(msg.text());
+    // });
 
     const workbook = new xl.Workbook();
-    // await workbook.xlsx.readFile('acmodasi.xlsx');
+    await workbook.xlsx.readFile('acmodasi.xlsx');
+    let worksheet = workbook.getWorksheet(1);
 
-    let collectedData = [];
     await page.setCookie(
         {
             "name": "acmodasi",
@@ -57,10 +60,83 @@ async function analizeSites() {
             "session": false,
         }
     );
-    await page.goto('https://www.acmodasi.ru/index.php?action=search&todo=advanced&base=&un=&vo=4&vd=17&pvo=&pvd=&us=0&uc=&c=219&eda=0&ac=0&edm=0&mo=0&edd=0&da=0&eds=0&si=0&bt=&et=&hf=&ht=&wf=&wt=&hc=0&lf=0&lt=0&eye=0&cf=&ct=&sf=&st=&pp=3&on=1');
 
+    for (let i = 10, row = 1; i <= countOfPages; i++) {
+        await page.goto('https://www.acmodasi.ru/index.php?action=search&todo=advanced&base=&un=&vo=4&vd=17&pvo=&pvd=&us=0&uc=&c=219&eda=0&ac=0&edm=0&mo=0&edd=0&da=0&eds=0&si=0&bt=&et=&hf=&ht=&wf=&wt=&hc=0&lf=0&lt=0&eye=0&cf=&ct=&sf=&st=&pp=3&on=1&sp=0&id=0&ph=0&p=' + i);
 
+        let ankets = await page.$$('.user_card_image_box');
+        for (let anket of ankets) {
+            await anket.click();
+            await page.waitForTimeout(2000);
+            let child = {};
+            let tabContent = await page.$('#pills-tabContent .col-md-6.col-sm-12.mt-4');
+            child['name'] = await tabContent.$$eval('div.row', (elements) => {
+                for (let e of elements) {
+                    let propName = e.querySelector('div.table_label').innerHTML;
+                    if (propName === 'ФИО') {
+                        return e.querySelector('div.table_value').innerHTML;
+                    }
+                }
+            });
+            child['gender'] = await tabContent.$$eval('div.row', (elements) => {
+                for (let e of elements) {
+                    let propName = e.querySelector('div.table_label').innerHTML;
+                    if (propName === 'Пол') {
+                        return e.querySelector('div.table_value').innerHTML;
+                    }
+                }
+            });
+            await page.click('#p_contacts');
+            await page.waitForTimeout(1000);
 
+            if (await page.evaluate(() => { return !!document.getElementById("show_contact_buton") })) {
+                await page.click('#show_contact_buton');
+            }
+            await page.waitForTimeout(500);
+            let contactRows = await page.$$('div#user_contacts div.row');
+            child['phone'] = null;
+            for (let contactRow of contactRows) {
+                let nameOfService = await contactRow.$eval('div.table_label', (e) => e.innerHTML);
+                if (nameOfService === 'Whatsapp' || nameOfService === 'Телефон:' || nameOfService === 'Viber') {
+                    child['phone'] = await (await contactRow.$eval('div.table_value a', (e) => e.innerHTML)).replace(/\s|\(|\)|-/g, '');
+                    break;
+                }
+            }
+            if (child.hasOwnProperty('name') && child.hasOwnProperty('gender') && child.hasOwnProperty('phone')) {
+                worksheet.getCell(row, 1).value = child['name'];
+                worksheet.getCell(row, 2).value = child['phone'];
+                worksheet.getCell(row, 3).value = child['gender'];
+            }
+            row++;
+            // let form = page.$eval('form input.btn.orange_button.mt-2[type=submit]', (e) => e.parentElement);
+            // await form.type('textarea.form-control', 'Приглашаем молодого музыканта на конкурс. Подробности: kidfest.ru');
+            // await form.click('textarea.form-control');
+            await page.click('div.modal-footer button.btn');
+            await page.waitForTimeout(1000);
+        }
+        await console.log('Page number ' + i + ' completed!');
+        break;
+    }
+    await workbook.xlsx.writeFile('acmodasi.xlsx');
+
+    await fs.readFile("./acmodasi.xlsx", function (err, data) {
+        let transporter = mailer.createTransport({
+            host: 'smtp.yandex.ru',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'iroge-27@yandex.ru',
+                pass: 'V04@r70n1V'
+            }
+        });
+        transporter.sendMail({
+            sender: 'iroge-27@yandex.ru',
+            to: 'iroge-27@yandex.ru',
+            subject: 'Данные acmodasi',
+            body: '',
+            attachments: [{'filename': 'acmodasi.xlsx', 'content': data}]
+        });
+    });
 }
 
 function serialize(obj, prefix)
